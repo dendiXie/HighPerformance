f5c4852415f8aca26b773515d513d7e404608d17
dendiXie <13818416042@163.com>
2019-10-13 14:33:41 +0800

    init

---
+++ src/main/java/com/highperformance/HighperformanceApplication.java
@@ -0,0 +1,13 @@
+package com.highperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighperformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighperformanceApplication.class, args);
+    }
+
+}
